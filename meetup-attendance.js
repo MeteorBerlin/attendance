f271370eabbcb376f5186418c021682157d2b093
Nicholas Kelly <eiliyasx@gmail.com>
2015-11-02 09:26:49 +0100

    Another change (monday)
    A late start. Beginning to play around with the radio buttons and forms
    for questionnaire

--- meetup-attendance.js
+++ meetup-attendance.js
@@ -1,6 +1,30 @@
 if (Meteor.isClient) {
+
+//alert(document.meteorites.user.checked);
   // counter starts at 0
   Session.setDefault('counter', 0);
+
+
+  function collectData(){
+    var user 
+   = document.getElementsByName("user");
+
+   for (var i = 0; i < user.length; i++) {
+    if (user[i].checked) {
+      alert(user[i].value);
+      break;
+    };
+   }
+}
+
+
+
+
+
+
+  //var skill = document.getElementById("skill").value;
+
+  //alert(skill);
 
   Template.hello.helpers({
     counter: function () {
@@ -16,9 +40,20 @@ if (Meteor.isClient) {
   });
 }
 // code for questionnaire begins
-var userData = prompt("Do you use Meteor?")
-console.log(userData)
+//var userData = prompt("Do you use Meteor?")
+//console.log(userData)
 
+//var userData = prompt("Are you a founder?")
+
+//var userData = prompt("Do you use Meteor?")
+//if (userData = yes){
+  //ask another question
+       //some code
+//}
+//else{
+  //then ask another
+        //some code
+//}
 
 
 
